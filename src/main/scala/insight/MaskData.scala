d11b325fc2a690101adc7326ef03310e2fa2bde0
Tanjin Panna <tanjin.panna@gmail.com>
2018-01-28 15:11:45 -0500

    one 'call' with function chaining for entire application

--- src/main/scala/insight/MaskData.scala
+++ src/main/scala/insight/MaskData.scala
@@ -22,20 +22,13 @@ object MaskData {
 
     // reading data from s3
     // TODO figure out how to loop through config to generate multiple dataframes
-    val df = spark.read
+    spark.read
       .format("com.databricks.spark.csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .load(s"s3n://$bucketName/csv/activity-logs.csv") // TODO parameterize the time stamp
-
-    // apply masking logic
-    // TODO be dynamic in the type of mask (hardcoded to String here)
-    val masked = df.withColumn("status", lit("MASKED"))
-
-    // writing back to s3
-    masked.coalesce(1).write // 1 could be bad?
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
+      .withColumn("status", lit("MASKED")) // TODO be dynamic in the type of mask (hardcoded to String here)
+      .write
       .mode("append")
       .save(s"s3n://$bucketName/data/csv")
 
