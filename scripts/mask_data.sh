a1b862ca48eed48afc011e8d22c2d4b21b31039a
Ubuntu <ubuntu@ip-10-0-0-5.ec2.internal>
2018-01-29 17:29:20 +0000

    adding scripts directory to contain shell scripts

---
+++ scripts/mask_data.sh
@@ -0,0 +1,5 @@
+echo "Beginning spark application"
+
+export SPARK_HOME
+cd ~/dev/sparkMasking
+$SPARK_HOME/bin/spark-submit --class insight.MaskData --master spark://ip-10-0-0-5.ec2.internal:7077 target/scala-2.11/masking-data_2.11-0.1.jar vts-dummyData
