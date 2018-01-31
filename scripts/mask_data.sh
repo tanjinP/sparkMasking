9fd2cc86b3016f0a336241384643f76439ea0288
Tanjin Panna <tanjin.panna@gmail.com>
2018-01-31 06:17:23 -0500

    spark script taking into account new changes (conf file and jar)

--- scripts/mask_data.sh
+++ scripts/mask_data.sh
@@ -1,7 +1,13 @@
-echo "Beginning spark application"
-
+#!/bin/bash
 export SPARK_HOME
+
+echo "Beginning Spark application"
+
 cd ~/dev/sparkMasking
-$SPARK_HOME/bin/spark-submit --class insight.MaskData --master spark://ip-10-0-0-5.ec2.internal:7077 target/scala-2.11/masking-data_2.11-0.1.jar vts-dummyData
+$SPARK_HOME/bin/spark-submit --class insight.MaskData \
+--master spark://ip-10-0-0-5.ec2.internal:7077 \
+--files src/main/resources/application.conf \
+--jars jars/config-1.3.1.jar \
+target/scala-2.11/masking-data_2.11-0.1.jar
 
 echo "Spark job is complete"
