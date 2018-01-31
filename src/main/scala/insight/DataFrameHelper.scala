ab2fc8244bb31ce4514441a6b69367937b45a973
Tanjin Panna <tanjin.panna@gmail.com>
2018-01-31 06:14:25 -0500

    implicit class containing multipleColumns def

---
+++ src/main/scala/insight/DataFrameHelper.scala
@@ -0,0 +1,14 @@
+package insight
+
+import org.apache.spark.sql.{Column, DataFrame}
+
+object DataFrameHelper {
+  implicit class RichDataFrame(df: DataFrame) {
+    // utilizing foldLeft to conduct `.withColumn` on single DataFrame multiple times
+    def withMultipleColumns(columns: (String, Column)*): DataFrame = {
+      columns.foldLeft(df) {case (dataframe, (field, column)) =>
+        dataframe.withColumn(field, column)
+      }
+    }
+  }
+}
